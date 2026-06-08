372ee600d41bcb956a662ee3b6ed216fb2dd2c82
Harshcodes04 <harshitjantwal0418@gmail.com>
2026-06-09 02:13:09 +0530

    created basic ui of login page

--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,5 +1,40 @@
+import "../auth.form.scss";
+import { useNavigate, Link } from "react-router";
+
 const login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <main>
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="email here"
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="password here"
+            />
+            <button className="button primary-button">Login</button>
+          </div>
+        </form>
+        <p>
+          Don't have an account? <Link to="/register">Register</Link>
+        </p>
+      </div>
+    </main>
+  );
 };
 
 export default login;
