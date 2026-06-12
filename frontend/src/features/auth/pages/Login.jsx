e7b52e4f964526b8ff0cf30b223f35fa38e56c52
Harshcodes04 <harshitjantwal0418@gmail.com>
2026-06-12 22:47:25 +0530

    added hooks in login

--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,11 +1,25 @@
 import "../auth.form.scss";
 import { useNavigate, Link } from "react-router";
+import { useAuth } from "../hooks/useAuth";
+import { React, useState } from "react";
 
 const login = () => {
+  const { loading, handleLogin } = useAuth();
+  const { email, setEmail } = useState("");
+  const { password, setPassword } = useState("");
   const navigate = useNavigate();
-  const handleSubmit = (e) => {
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    await handleLogin({ email, password });
   };
+  if (loading) {
+    return (
+      <main>
+        <h1>Loading... </h1>
+      </main>
+    );
+  }
   return (
     <main>
       <div>
@@ -18,6 +32,9 @@ const login = () => {
               id="email"
               name="email"
               placeholder="email here"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
             />
             <label htmlFor="password">Password</label>
             <input
@@ -25,6 +42,9 @@ const login = () => {
               id="password"
               name="password"
               placeholder="password here"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
             />
             <button className="button primary-button">Login</button>
           </div>
